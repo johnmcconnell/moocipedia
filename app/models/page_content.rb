9e651c2a24a14f5f4efabe2bdbda916d8bc83f3a
John McConnell <johnnyillinois@gmail.com>
2016-04-19 08:59:24 -0700

    embedded video works

--- app/models/page_content.rb
+++ app/models/page_content.rb
@@ -15,11 +15,44 @@ class PageContent < ActiveRecord::Base
   private
 
   def render_html
-    self.html = renderer.render(content)
+    self.html = markdown_render(content)
+  end
+
+  def markdown_render(content)
+    html = renderer.render(content)
+
+    doc = Nokogiri::HTML(html)
+
+    doc.search("//p[contains(text(),'@video')]").each do |match|
+      url = nil
+      done = false
+      match.children.each do |child|
+        if done
+          next
+        end
+
+        if 'a' == child.name
+          url = child.attributes['href'].value
+          match.replace video_template(url)
+          done = true
+          break
+        end
+      end
+    end
+
+    doc.to_s
+  end
+
+  def video_template(url)
+%(<div class="col-xs-12">
+  <div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" src="#{url}" frameborder="0" allowfullscreen></iframe>
+  </div>
+</div>)
   end
 
   def renderer
-    Redcarpet::Markdown.new(
+   Redcarpet::Markdown.new(
       Redcarpet::Render::HTML.new(escape_html: true),
       autolink: true, tables: true,
     )
