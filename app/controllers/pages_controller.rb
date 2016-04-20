4ae6fe856eb90c3e49e84ad65e0e098d79df696b
John McConnell <johnnyillinois@gmail.com>
2016-04-20 00:30:04 -0700

    allow page deletions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,19 @@
 class PagesController < ApplicationController
-  before_action :set_page, only: [:show, :edit, :move_higher, :move_lower]
+  before_action :set_page, only: [
+    :show, :edit, :move_higher, :move_lower, :destroy
+  ]
   respond_to :html
 
   def show
   end
 
   def edit
+  end
+
+  def destroy
+    @page.remove_from_list
+    @page.delete
+    redirect_to edit_pages_lesson_path(@page.lesson)
   end
 
   def move_higher
